6073b70c6e7d2f588cca784b291f2f054c36faf2
rLapz <arthurlapz@gmail.com>
2021-02-21 22:54:52 +0700

    modified:   hexlib.c 	modified:   hexlib.h 	new file:   test.c

--- hexlib.c
+++ hexlib.c
@@ -1,5 +1,29 @@
 #include "hexlib.h"
 
+
+static int hex_val(int c)
+{
+	switch(c) {
+		case '0': return 0;
+		case '1': return 1;
+		case '2': return 2;
+		case '3': return 3;
+		case '4': return 4;
+		case '5': return 5;
+		case '6': return 6;
+		case '7': return 7;
+		case '8': return 8;
+		case '9': return 9;
+		case 'a': case 'A': return 10;
+		case 'b': case 'B': return 11;
+		case 'c': case 'C': return 12;
+		case 'd': case 'D': return 13;
+		case 'e': case 'E': return 14;
+		case 'f': case 'F': return 15;
+	}
+	return -1;
+}
+
 void int_to_hex(char* dest, const char* format,
 		int* value, size_t len)
 {
@@ -20,28 +44,34 @@ void str_to_hex(char* dest,
 	strcpy(dest, buffer);
 }
 
-void hex_to_byte(unsigned char* dest,
-		char* value, size_t len)
+void hex_to_byte(unsigned char* dest, const char* value)
 {
-	char* pos = value;
-	char* endptr;
-	size_t count = 0;
-
-	for (count = 0; count < len; count++) {
-		char buf[5] = {'0', 'x', pos[0], pos[1], '\0'};
-		dest[count] = strtol(buf, &endptr, 0);
-		pos += 2 * sizeof(char);
+	size_t len = strlen(value);
+	int c;
+	if (len&1) {
+		*(dest++) = hex_val(*value);
+		*value++;
+		len++;
+	}
+	while (*value) {
+		c = hex_val(*value);
+		value++;
+		c = c << 4;
+		c |= hex_val(*value);
+		value++;
+		*dest = c;
+		dest++;
 	}
 }
 
-void byte_to_hex(char* dest, const char *format,
-		unsigned char* value, size_t len)
+void byte_to_hex(char* dest,
+		const unsigned char* value, size_t len)
 {
-	char buffer[len];
 	size_t i, j;
-
-	for (i = 0, j = 0; i < len; i++, j += 2)
-		sprintf(j > 0 ? buffer+j : buffer, format, value[i]);
-
-	strcpy(dest, buffer);
+	static const char hex[] = "0123456789abcdef";
+	for (i = 0, j = 0 ; i < len; i++, j += 2) {
+		dest[j + 0] = hex[0xf & (value[i] >> 4)];
+		dest[j + 1] = hex[0xf & value[i]];
+	}
+	dest[j] = 0;
 }
--- hexlib.h
+++ hexlib.h
@@ -9,9 +9,8 @@ void int_to_hex(char* dest, const char* format,
 		int* value, size_t len);
 void str_to_hex(char* dest, const char* value,
 		size_t len);
-void hex_to_byte(unsigned char* dest, char* value,
+void hex_to_byte(unsigned char* dest, const char* value);
+void byte_to_hex(char* dest, const unsigned char* value,
 		size_t len);
-void byte_to_hex(char* dest, const char *format,
-		unsigned char* value, size_t len);
 
 #endif
