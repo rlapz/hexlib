0392787725244623dde62bbf202c2fbebb570916
rLapz <arthurlapz@gmail.com>
2021-05-01 13:23:31 +0700

    modified:   main.c 	modified:   ../hexlib.c

--- test/main.c
+++ test/main.c
@@ -7,7 +7,7 @@ main(void)
 	uint32_t hex = 0xFF;
 
 	printf("%d\n", byte2dec(&hex, sizeof(tmp)));
-	printf("%s\n", dec2hex(tmp, 100, sizeof(tmp)));
+	printf("%s\n", dec2hex(tmp, 2000000000, sizeof(tmp)));
 
 	int8_t byte[100];
 	uint8_t h[] = {0xFF, 0xFF, 0xEE};
@@ -15,10 +15,12 @@ main(void)
 	printf("%s\n", byte);
 
 	uint8_t b[3];
-	int8_t test[] = "61";
+	int8_t test[] = "616364";
 
 	hex2byte(b, test);
 	printf("%c\n", b[0]);
+	printf("%c\n", b[1]);
+	printf("%c\n", b[2]);
 	
 
 	return 0;
